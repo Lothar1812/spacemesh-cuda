89585a32276f8cfba28ac6f0a5c417cf64b82c1b
Lothar1812 <48362336+Lothar1812@users.noreply.github.com>
2024-06-11 00:40:14 +0200

    Update build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -10,16 +10,16 @@ fn main() {
     let mut dst = Config::new("..")
         .define("CMAKE_BUILD_TYPE", profile)
         .define("WITH_TEST", "OFF")
-        .cxxflag("-O3")
+        .cxxflag("/O2") // Anpassung für Windows-Compiler-Flag
         .build();
     dst.push("lib");
     println!("cargo:rustc-link-search=native={}", dst.display());
 
-    let default_cuda_lib_path = "/usr/local/cuda/targets/x86_64-linux/lib/";
-    let default_boost_path = "/usr/local/lib/";
+    let default_cuda_lib_path = "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.2\\lib\\x64";
+    let default_boost_path = "C:\\local\\boost_1_75_0\\lib64-msvc-14.2";
     println!("cargo:rustc-link-search=native={}", default_cuda_lib_path);
     println!("cargo:rustc-link-search=native={}", default_boost_path);
     println!("cargo:rustc-link-lib=static=cudart_static");
-    println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=msvcrt"); // Anpassung für Windows: msvcrt statt stdc++
     println!("cargo:rustc-link-lib=static=spacemesh-cuda");
 }
